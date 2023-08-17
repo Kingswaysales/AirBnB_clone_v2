11e2b706214c146c12a178836f24ef8c769ce5fd
kingswaysales <marco.nkululeko@gmail.com>
2023-08-17 11:45:48 -0700

    task4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""Starts Flask web app
-Routes:
-    / - display "Hello HBNB!"
-    /hbnb - display "HBNB"
-    /c/<text> - display "C <text>"
-    /python/<text> - display "Python is cool"
-    /number/<n> - display n if integer
-"""
-from flask import Flask
+if __name__ == "__main__":
+    from flask import Flask
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
 
-app = Flask(__name__)
+    @app.route('/')
+    def hello_HBNB():
+        return 'Hello HBNB!'
 
+    @app.route('/hbnb')
+    def hbnb():
+        return 'HBNB'
 
-@app.route('/', strict_slashes=False)
-def hbnb_route():
-    """prints Hello HBNB"""
-    return "Hello HBNB!"
+    @app.route('/c/<text>')
+    def c_is(text):
+        return "C {}".format(text.replace('_', ' '))
 
+    @app.route('/python', defaults={'text': 'is cool'})
+    @app.route('/python/<text>')
+    def python_is(text):
+        return 'Python {}'.format(text.replace('_', ' '))
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """prints HBNB"""
-    return "HBNB"
+    @app.route('/number/<int:n>')
+    def number(n):
+        return '{} is a number'.format(n)
+
+    app.run(host='0.0.0.0', port=5000)

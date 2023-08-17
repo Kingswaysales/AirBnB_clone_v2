44a84c59f0b25f0500c3dfef85f61ad0feeb5c86
kingswaysales <marco.nkululeko@gmail.com>
2023-08-17 11:33:33 -0700

    task4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+    /number/<n> - display n if integer
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints HBNB"""
+    return "HBNB"

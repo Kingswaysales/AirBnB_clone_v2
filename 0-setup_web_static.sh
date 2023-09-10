a922bf9d935034a62fa7e18d463491555081726c
kingswaysales <marco.nkululeko@gmail.com>
2023-09-10 12:12:39 -0700

    task0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,48 @@
 #!/usr/bin/env bash
-# Prepare web servers for deployment
+# sets up my web servers for the deployment of web_static
+
+echo -e "\e[1;32m START\e[0m"
+
+#--Updating the packages
 sudo apt-get -y update
 sudo apt-get -y install nginx
+echo -e "\e[1;32m Packages updated\e[0m"
+echo
 
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+#--configure firewall
+sudo ufw allow 'Nginx HTTP'
+echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
+echo
 
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
+#--created the dir
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "\e[1;32m directories created"
+echo
 
+#--adds test string
+echo "<h1>Welcome to www.smartcodez.tech</h1>" > /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Test string added\e[0m"
+echo
+
+#--prevent overwrite
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
+echo -e "\e[1;32m prevent overwrite\e[0m"
+echo
+
+#--create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
 
-sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
-sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+echo -e "\e[1;32m Symbolic link created\e[0m"
+echo
 
+#--restart NGINX
 sudo service nginx restart
+echo -e "\e[1;32m restart NGINX\e[0m"
